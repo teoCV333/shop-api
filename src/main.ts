755b9df17095b0b6c84a78c02eee5237ff4a8223
MCVdev <mateocarvajalvergara333@gmail.com>
2025-12-01 21:33:00 -0500

    db migration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://zentrashop.com.co',
   });
 
   await app.listen(process.env.PORT ?? 3000);
