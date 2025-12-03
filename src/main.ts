62cba18d02b6393b6d5376c462dffe084164b0d6
MCVdev <mateocarvajalvergara333@gmail.com>
2025-12-02 20:32:10 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ console.log('>>> Arrancando aplicación Nest en servidor...');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  /* app.enableCors({
+    origin: '*',
+  }); */
   app.enableCors({
-    origin: 'https://zentrashop.com.co',
+    origin: 'https://zentrastorecol.com',
   });
 
   await app.listen(process.env.PORT ?? 3000);
